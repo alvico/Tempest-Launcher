054b4e13e2b71969b6ad81cb648b73590494dd86
Albert Vico <albert.vico@midokura.com>
2014-08-07 11:51:00 +0200

    fix

--- fabfile.py
+++ fabfile.py
@@ -7,19 +7,20 @@ from pprint import pprint
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 env.user = "root"
-env.password = "gogomid0"
+env.password = ""
 cuisine.select_package('yum')
 
 
 def setup_tempest():
     """
-change tempest fork
-checkout tempest branch
-pull code
-"""
+    change tempest fork
+    checkout tempest branch
+    pull code
+    """
     with cd('/var/lib/tempest/'):
         run('git remote add midokura git@github.com:midokura/tempest.git ')
         run('git fetch midokura')
+        run('git checkout albert')
         run('git branch --set-upstream albert midokura/albert')
         run('git pull')
 
@@ -28,14 +29,16 @@ def pull_tempest():
     """
     TODO: Check git is on the correct tempest branch
     pull the code
-"""
+    """
     with cd('/var/lib/tempest/'):
         run('git pull')
 
 
 def run_tempest():
+    if cuisine.dir_exists("~/tempest/bin/"):
+     run("source tempest/bin/activate")
     pull_tempest()
-    tests = "test_network_basic_vmconnectivity"
+    tests = "test_network_basic_inter_vmconnectivity"
     try:
         with cd('/var/lib/tempest/tempest/scenario/midokura/'):
            run('nosetests -q {0} --logging-level=INFO'.format(tests))
