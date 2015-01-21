5d514190045af42c86c390574df2b6a6cd73f45d
alvico <albert.vico@midokura.com>
2015-01-21 13:01:54 +0100

    tempest launcher with ansible v1

--- fabfile.py
+++ fabfile.py
@@ -2,64 +2,64 @@ import cuisine
 from fabric.api import *
 from fabric.colors import green, red, blue
 import logging
-from pprint import pprint
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 env.user = "root"
 env.password = "gogomid0"
-cuisine.select_package('yum')
+cuisine.select_package('apt')
 
+def pre_req():
+    cuisine.package_update_apt()
+    sudo('apt-get upgrade')
+    cuisine.package_ensure('wget')
+    cuisine.package_ensure('gcc')
+    cuisine.package_ensure('python-dev')
+    cuisine.package_ensure('python-pip')
+    cuisine.package_ensure('wget')
+    cuisine.package_ensure('gcc')
+    cuisine.package_ensure('libffi-dev')
+    cuisine.package_ensure('libssl-dev')
+    cuisine.package_ensure('libxml2-dev')
+    cuisine.package_ensure('libxslt-dev')
+    cuisine.package_ensure('git')
 
-def setup_tempest():
-    """
-    change tempest fork
-    checkout tempest branch
-    pull code
-    """
-    with cd('/var/lib/tempest/'):
-        run('git remote add mine git@github.com:alvico/tempest.git ')
-        run('git fetch mine')
-        run('git checkout albert')
-        #RHEL git branch --set-upstream foo upstream/foo
-        run('git branch -u mine/albert')
-        run('git pull')
+def set_up_key():
+    run('ssh-keygen -t rsa -C "your_email@example.com"')
+    run('sh-add ~/.ssh/id_rsa')
+    run('cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys')
 
+def clone_qa():
+    # Make sure we hace git access rights
+    run('git clone https://github.com/midokura/qa.git')
+    with cd('~/qa'):
+        run('git submodule update --init')
 
-def pull_tempest():
-    """
-    TODO: Check git is on the correct tempest branch
-    pull the code
-    """
-    with cd('/var/lib/tempest/'):
-        run('git pull')
+def execute_ansible():
+    with cd('~/qa/tools/ansible'):
+        run('ansible-playbook -vvvv -i hosts_localhost_allinone
+                local-allinone.yml -e deploy=ubuntu14 -e midonet_version=2015.01
+                -e openstack_version=juno')
 
-
-def switch_branch():
-    with cd('/var/lib/tempest/'):
-        run('git checkout albert')
-
+def tempest_preliminars():
+    cuisine.python_package_ensure_pip('virtualenv')
+    cuisine.python_package_ensure_pip('virtualenvwrapper')
+    run('echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc')
+    
+def tempest_setup():
+    with cd('~'):
+        run('git clone https://github.com/midokura/tempest.git')
+    with cd('~/tempest')
+        sudo('cp /root/tempest.conf ~/tempest/etc/')
+        sudo('chmod  a+rw ~/tempest/etc/tempest.conf')
+        run('git fetch --all')
+        run('git checkout development')
+        with prefix('workon tempest'):
+            cuisine.python_package_install_pip(r='requirements.txt')
+            run('python mido-setup.py')
 
 def run_tempest():
-    if cuisine.dir_exists("~/tempest/bin/"):
-     run("source tempest/bin/activate")
-    pull_tempest()
-    tests = "test_network_basic_inter_vmconnectivity"
-    try:
-        with cd('/var/lib/tempest/tempest/scenario/midokura/'):
-           run('nosetests -q {0} --logging-level=INFO'.format(tests))
-    except ValueError:
-        print "Oops!  That was a FAIL.  Try again..."
-
-def run_all_custom():
-    tests = "test_network_basic_adminstateup " \
-            "test_network_basic_multisubnet " \
-            "test_network_basic_metadata " \
-            "test_network_basic_vmconnectivity " \
-            "test_network_basic_inter_vmconnectivity "
-    try:
-        run("source /root/tempest/bin/activate")
-        with cd('/var/lib/tempest/tempest/scenario/midokura/'):
-           run('nosetests -q {0} --logging-level=INFO'.format(tests))
-    except ValueError:
-        print "Oops!  That was a FAIL.  Try again..."
+    with prefix('workon tempest'):
+        run('./run_tempest.sh tempest.api.network 
+                tempest.scenario.test_network_basic_ops 
+                tempest.scenario.test_network_advanced_server_ops 
